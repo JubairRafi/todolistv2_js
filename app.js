e3869d366c556d858732d7673fa3ce528ae09fb5
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-25 09:03:51 -0700

    daynamic port for heroku

--- app.js
+++ app.js
@@ -180,10 +180,12 @@ app.post("/delete",(req,res)=>{
 //   res.render("list", {listTitle: "Work List", newListItems: workItems});
 // });
 
+let port = process.env.PORT;
+if(port == null || port ==""){
+  port = 3000;
+}
 
 
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started succesfully");
 });
